f584c1d361d915edfa2b18a721e693f20d21c420
Reiker7 <reiker1@gmail.com>
2025-05-24 23:25:59 +0200

    240525_min64

---
+++ course/tipos/01-variables.py
@@ -0,0 +1,11 @@
+nombre_curso = "Ultimate Python"
+nombre1 = "hola"
+NOMBRE_CURSO = "Ultimate Python"
+Nombre_Curso = "Ultimate Python"
+NombreCurso = "Ultimate Python"
+
+alumnos = 5000
+puntuaje = 9.9
+publicado = True
+
+print(nombre_curso)
---
+++ course/tipos/03-format-strings.py
@@ -0,0 +1,8 @@
+nombre = "Nicolas"
+apellido = "Shurmann"
+nombre_completo = nombre + " " + apellido
+nombre_completo2 = f"{nombre} {apellido}"
+nombre_completo3 = f"{nombre[0]} {2 + 5}"
+print(nombre_completo)
+print(nombre_completo2)
+print(nombre_completo3)
